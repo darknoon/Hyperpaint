51824871ecf7ab0e821d7bb40c68e5125b27ba0d
Andrew Pouliot <andrew@darknoon.com>
2022-12-03 12:35:15 -0500

    Initial Commit

---
+++ Hyperpaint/HyperpaintApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyperpaintApp.swift
+//  Hyperpaint
+//
+//  Created by Andrew Pouliot on 12/3/22.
+//
+
+import SwiftUI
+
+@main
+struct HyperpaintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
